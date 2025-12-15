2708cd1351b6fe89cbeafb815ce6e817d5c854bb
maxthemusicnerd <156611140+maxthemusicnerd@users.noreply.github.com>
2025-12-14 20:17:35 -0700

    added google oauth

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -4,6 +4,57 @@ import bcrypt
 from azure.storage.blob import BlobServiceClient
 import io
 
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+
+app.secret_key = "925449f1e7c49251e1e6aa8d041981e30b966a0b345dc3a0af9178949364bd82"
+
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+#Google login stuff
+google_bp = make_google_blueprint(
+    client_id="258097526774-i760aq66967p8jds4m1220loj6skj1qi.apps.googleusercontent.com",
+    client_secret="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",
+    scope=[
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid"
+    ],
+    redirect_url="/google_login" 
+)
+
+app.register_blueprint(google_bp, url_prefix="/login")
+
+
+@app.route("/google_login")
+def google_login():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+
+    resp = google.get("/oauth2/v2/userinfo")
+    if not resp.ok:
+        return redirect(url_for("login_page"))
+
+    info = resp.json()
+    username = info["email"]
+
+    users = load_users()
+    if username not in users["username"].values:
+        new_row = {"username": username, "password_hash": ""}
+        users = users._append(new_row, ignore_index=True)
+        save_users(users)
+
+    #manually set session
+    session["user"] = username
+    return redirect(url_for("home"))
+
+
+
+
+
 
 azure_key = "DefaultEndpointsProtocol=https;AccountName=hashaccount;AccountKey=ZHW+Pgh3i7A8kgulaMviIjJpOiuYm7O2em9SfZTekfBfwED6WboU+MFsSo+ER6SX4hDMaH2tQizP+AStNI5k3g==;EndpointSuffix=core.windows.net"
 
@@ -33,9 +84,7 @@ def save_users(df):
 
     csv_data = df.to_csv(index=False)
     blob_client.upload_blob(csv_data, overwrite=True)
-app = Flask(__name__)
 
-app.secret_key = "925449f1e7c49251e1e6aa8d041981e30b966a0b345dc3a0af9178949364bd82"
 
 @app.route('/home')
 def home():
